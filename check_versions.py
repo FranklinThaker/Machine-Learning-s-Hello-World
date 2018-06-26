ed40f4e6886debc266e550d57bde9b094d6b32ee
Jarvis <franklin873@live.com>
2018-06-26 11:43:58 +0530

    first commit

---
+++ check_versions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 25 21:18:58 2018
+
+@author: Franklin P. Thaker (franklin873@live.com)
+"""
+import sys
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+
+print("Python:",format(sys.version))
+print("Scipy:", format(scipy.__version__))
+print("Numpy:", format(numpy.__version__))
+print("Matplotlib:",format(matplotlib.__version__))
+print("Pandas:",format(pandas.__version__))
+print("Sklearn:",format(sklearn.__version__))
